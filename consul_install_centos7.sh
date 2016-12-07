9d17af2c84c4f7bcc8f04863e79b0dda1ac29054
Gregory H Shepherd <gshepherd@solidyn.com>
2016-12-07 14:11:11 -0700

    added more firewall ports

--- consul_install_centos7.sh
+++ consul_install_centos7.sh
@@ -15,6 +15,28 @@ mv consul /usr/local/bin/
 mv index.html /home/consul/www/
 cp static/ /home/consul/www/
 
+sudo firewall-cmd --add-port=8300/tcp
+sudo firewall-cmd --add-port=8300/tcp --permanent
+sudo firewall-cmd --add-port=8400/tcp
+sudo firewall-cmd --add-port=8400/tcp --permanent
+sudo firewall-cmd --add-port=8500/tcp
+sudo firewall-cmd --add-port=8500/tcp --permanent
+sudo firewall-cmd --add-port=8600/tcp
+sudo firewall-cmd --add-port=8600/tcp --permanent
+sudo firewall-cmd --add-port=8600/udp
+sudo firewall-cmd --add-port=8600/udp --permanent
+sudo firewall-cmd --add-port=8301/tcp
+sudo firewall-cmd --add-port=8301/tcp --permanent
+sudo firewall-cmd --add-port=8302/tcp
+sudo firewall-cmd --add-port=8302/tcp --permanent
+sudo firewall-cmd --add-port=8301/udp
+sudo firewall-cmd --add-port=8301/udp --permanent
+sudo firewall-cmd --add-port=8302/udp
+sudo firewall-cmd --add-port=8302/udp --permanent
+
+
+
+
 mkdir -p /etc/consul.d/bootstrap
 mkdir -p /etc/consul.d/server
 cd ~centos/consul_config
